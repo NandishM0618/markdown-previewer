605a4dd87aad06a3a28739c0a4ebccbed59ab067
Nandish <nandish1729@gmail.com>
2023-11-21 18:11:51 +0530

    markdown-previewer freecodeCamp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { marked } from "marked";
+import { useState } from "react";
 
-function App() {
+export default function App(params) {
+  const [markdown, setMarkdown] = useState("# Your Markdown Here");
+
+  const handleInputChange = (e) => {
+    setMarkdown(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="editor">
+        <h2>Editor</h2>
+        <textarea value={markdown} onChange={handleInputChange} />
+      </div>
+      <div className="preview">
+        <h2>Preview</h2>
+        <div dangerouslySetInnerHTML={{ __html: marked(markdown) }} />
+      </div>
     </div>
   );
 }
-
-export default App;
